3925d012fd6cd41ac3cbf542c4e003792ba2a533
suh park <suhpark@gmail.com>
2020-03-24 20:41:56 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      currentTodo: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState.currentTodo += event.target.value;
+    console.log(event.target.value);
+    console.log(this.setState.currentTodo);
+  };
+
+  addItem = event => {
+    event.preventDefault();
+    console.log("the add item method executed");
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.addItem}>
+          <label htmlFor="taskName">Task Name:</label>
+          <input
+            onChange={this.handleChange}
+            name="taskName"
+            placeholder="Place your ToDo here"
+          />
+          <button type="submit">Add Task Here</button>
+        </form>
+        <ul></ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TodoList;
